b01bd08f27c1439c6c6f9fb38ca5203e388c1122
Sebi Secara <sebastian@wemakewebsites.com>
2020-09-19 18:17:29 -0700

    Removed manifest js

--- manifest.js
+++
@@ -1,1 +0,0 @@
-["static/chunks/0.js","static/chunks/amp.js","static/chunks/main.js","static/chunks/pages/_app.js","static/chunks/pages/_error.js","static/chunks/polyfills.js","static/chunks/proxy.js","static/chunks/react-refresh.js","static/chunks/storefront.js","static/css/styles.chunk.css","static/chunks/styles.js","static/chunks/webpack.js"]
